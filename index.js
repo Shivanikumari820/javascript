7b1ecb09f6fdd54629db654c1459130d9100e4e6
Shivanikumari820 <shivanikumari4218@gmail.com>
2025-03-29 18:36:32 +0530

    done

---
+++ index.js
@@ -0,0 +1,65 @@
+// External js
+var a = 15;
+var b = 9;
+
+console.log("sum of", a ," & " , b , "is:", a + b);
+console.log("sum of", a ," & " , b , "is:", a - b);
+console.log("sum of", a ," & " , b , "is:", a*b);
+console.log("sum of", a ," & " , b , "is:", a/b);
+
+
+/* like c++
+
+ int main(){
+
+    return 0;
+
+    okk()         //call the function 
+}
+
+*/
+
+
+/*    IN JAVASCRIPT ================>>>> Syntax
+function functionname(){
+
+    code----
+    console.log()
+}
+
+functioncall()    */
+
+// Example--->
+function Fun(){
+    var a = 4
+    var b= 3
+    console.log(4+3);
+}
+
+Fun()                                //calling the function
+
+
+function keyword(){
+    // re-declaration -------->>
+
+      var b= 3
+    // var a = 9                      // var allow re declaration in same scope
+      let a = 4
+    // let a = 9                      // let not allow re declaration in same scope
+      const c = 8;
+    // const c = 9;                     // const not allow redeclartion in same scope
+
+    //re-assignment ---------->>
+    var d =2;
+    var d = 2;                           //allowed
+
+    let d=3;
+    let d=3;                             // allowed 
+
+    const e= 3;                          
+    // const e= 3;                        // not allowed re -assignment
+    // console.log(d);
+}
+
+keyword()   
+
