ae8ca670abed60c75c287a133e41b526d3051c5b
Shivanikumari820 <shivanikumari4218@gmail.com>
2025-04-01 18:30:07 +0530

    done

--- index.js
+++ index.js
@@ -31,35 +31,56 @@ functioncall()    */
 
 // Example--->
 function Fun(){
-    var a = 4
-    var b= 3
-    console.log(4+3);
+    // var a = 4
+    // var b= 3
+    // console.log(4+3); 
 }
 
-Fun()                                //calling the function
+// Fun()                                //calling the function
 
 
 function keyword(){
+
     // re-declaration -------->>
 
-      var b= 3
-    // var a = 9                      // var allow re declaration in same scope
-      let a = 4
-    // let a = 9                      // let not allow re declaration in same scope
-      const c = 8;
-    // const c = 9;                     // const not allow redeclartion in same scope
+    //   var b= 3
+    // // var a = 9                      // var allow re declaration in same scope
+    //   let a = 4
+    // // let a = 9                      // let not allow re declaration in same scope
+    //   const c = 8;
+    // // const c = 9;                     // const not allow redeclartion in same scope
 
-    //re-assignment ---------->>
-    var d =2;
-    var d = 2;                           //allowed
+    // //re-assignment ---------->>
+    // var d =2;
+    // var d = 2;                           //allowed
 
-    let d=3;
-    let d=3;                             // allowed 
+    // let d=3;
+    // let d=3;                             // allowed 
 
-    const e= 3;                          
+    // const e= 3;                          
     // const e= 3;                        // not allowed re -assignment
     // console.log(d);
+
+
+    //scope{} 
+    // var is local scope
+    // let and const is global scope
 }
 
-keyword()   
+// keyword()   
+
+
+
+
+
+// hoisting = it is a concept in js in which variable declaration go on top just below the function name 
+
+function shivani (){
+/* 1. var k case me hoisting support krti h 
+  1. let k case me hoisting support nhi krti h 
+  1. const k case me hoisting support nhi  krti h  */
+  2. 
+
+  
+}
 
