4e8778bf2835f4ff57ca2e5543cc0f67784d26dc
Shivanikumari820 <shivanikumari4218@gmail.com>
2025-04-07 18:42:25 +0530

    done

--- DOM.js
+++ DOM.js
@@ -5,4 +5,8 @@
 function okk(){
     let show = document.querySelector('#text')     //Uses the DOM to select the element with id="text" and stores it in the variable show.
     show.innerHTML = "shivani"                     // innerHtml = Replaces the content inside that element with the text "shivani".
+    show.style.backgroundColor = "red"            // style 
+
+    // or
+    // document.querySelector('#text').show.innerHTML = "shivani"    // you
 }
