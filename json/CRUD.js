ccff1b86ed36cda009e3c9dd39c471a72ea3a4ef
Shivanikumari820 <shivanikumari4218@gmail.com>
2025-05-05 18:36:48 +0530

    done

--- json/CRUD.js
+++ json/CRUD.js
@@ -1,9 +1,11 @@
 let fetchData = async () => {                      
     let url = "http://localhost:3000/user";
 
-    let res = await fetch(url);
+    let res = await fetch(url,{method:"GET"});
     let data = await res.json();
 
+
+    //data jo db.json pr h usko table format me front screen pr show krne k liye use array of object => map 
     let dataShow = document.querySelector("#datashow");    
 
     data.map((e) => {
@@ -15,12 +17,14 @@ let fetchData = async () => {
             <td>${e.Check_out}</td>
             <td>${e.Person}</td>
             <td>${e.City}</td>
-            <td>${e.Price}</td>
+            <td>${e.Price*e.Person}</td>                                            
             <td onclick="Del('${e.id}')"> Delete </td>
         </tr>
         `;
     });
 };
+
+// person se price multiply hoga jo hamne price bhra h niche         
 
 fetchData();
 
@@ -30,4 +34,42 @@ let Del = (id) =>{
     // alert (id);
     let url = `http://localhost:3000/user/${id}`;
     fetch (url,{method:"DELETE"})                         //url ko fetch kiye or DELETE method lgaya , or ye json se bhi data delete kr dega
+    fetchData(); 
+}
+
+//how to send data in local storage --->
+
+let Book =() => {
+   let name =  document.querySelector("#name").value
+   let age =  document.querySelector("#age").value
+   let checkin =  document.querySelector("#checkin").value
+   let checkout =  document.querySelector("#checkout").value
+   let City =  document.querySelector("#city").value
+   let person =  document.querySelector("#person").value
+
+
+   
+    let url = "http://localhost:3000/user";
+    fetch(url,{method:"POST",                        //  postmethod me 3 chije bhejte h ===>>   method, header, string format me data 
+
+    headers:{
+        "content-type":"application/json"
+    },
+
+    body: JSON.stringify({
+        Name: name,                 //NAME (jo json me key h uski jgh vo value aajaye jo ham fomr me dalenge jo ki name h jo value utha rha h )        
+        Age: age,
+        Check_in: checkin,
+        Check_out: checkout,
+        City: City,
+        Person: person,
+        Price: 500
+      })
+      
+                              
+    });
+
+    location.href ="CRUD.html"
+    return false;
+
 }
