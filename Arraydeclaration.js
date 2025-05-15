109b43fe11a08e419294cf062cb634ed66f3ddb6
Shivanikumari820 <shivanikumari4218@gmail.com>
2025-05-15 21:58:52 +0530

    done

---
+++ Arraydeclaration.js
@@ -0,0 +1,12 @@
+// 1. Array Literal (Recommended and Most Common)
+const fruits = ["Apple", "Banana", "Mango"];
+
+// 2.  Array Constructor (new Array())
+const numbers = new Array(10, 20, 30);
+
+// 3. Creating an Array from a String (split() method)
+const data = "Red,Green,Blue";
+const colors = data.split(",");   // output : ["Red", "Green", "Blue"].
+
+// 4.  Using Array.of()
+const numbers1 = Array.of(1, 2, 3);   // output : [1, 2, 3].
